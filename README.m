01b99d6e903cc84cfe936a89f8d5ae5ddad08488
hab-same <136079566+hab-same@users.noreply.github.com>
2025-07-15 08:05:49 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,6 @@
 @@ -2,6 +2,8 @@
-<h1 align="center">Hi 👋, I'm Sadaam Huseen</h1>
-<h3 align="center">A Gravic designer & Software Engineer from somaliland</h3>
+<h1 align="center">Hi 👋, I'm Sadaam hussein</h1>
+<h3 align="center">A graphic designer & Software Engineer from somaliland</h3>
 
 <img align="right" alt="coding" width="400" src="https://user-images.githubusercontent.com/55389276/140866485-8fb1c876-9a8f-4d6a-98dc-08c4981eaf70.gif">
 
